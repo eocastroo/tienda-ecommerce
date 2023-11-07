fa50db52cb9159f972d4f22da03749737fc2816f
eocastroo <edwinorlandocastro@hotmail.com>
2023-11-07 14:45:42 -0500

    Se  realiza la lista de ordenes de compras, cssTailwind

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,3 +1,4 @@
+import {ChevronDoubleRightIcon} from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
 
@@ -5,12 +6,17 @@ const OrdersCard = props => {
 
     return(
 
-        <div className="flex justify-between items-center mb-3 border border-black">
-            <p>
-                <span> 02.11.23</span>
-                <span> {totalPrice}</span>
-                <span> {totalProducts}</span>
-            </p>
+        <div className="flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-80">
+            <div className="flex justify-between w-full">
+              <p className='flex flex-col'>
+                  <span className='font-light'> 02.11.23</span>
+                  <span className='font-light'> {totalProducts} Articles</span>
+              </p>
+              <p className='flex items-center gap-2'>
+                <span className='font-medium text-2xl'> ${totalPrice} </span>
+                <ChevronDoubleRightIcon className='h-6 w-6 text-black cursor-pointer' />
+              </p>        
+            </div>
         </div>
     )
 }
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -7,20 +7,23 @@ import Layout from "../../Components/Layout"
 
 function MyOrders() {
   const context = useContext(ShoppingCartContext)
+  console.log(context.order)
   return (
       <Layout>
-        <div className='flex items-center justify-center relative w-80'>
-          <h1>MyOrders</h1>
+        <div className='flex items-center justify-center relative w-80 mb-4'>
+          <h1 className='font-medium text-xl'>MyOrders</h1>
         </div>
          
          {
-          context.order.map((order, index) => {
-            <Link key={index} to={`/my-orders/${order.id}`}>
+          context.order.map((order, index) => (
+
+            <Link key={index} to={`/my-orders/${index}`}>
             <OrdersCard
             totalPrice={ order.totalPrice}
             totalProducts={order.totalProducts} />
             </Link>
-          })
+
+          ) )
          }
       </Layout>
         
