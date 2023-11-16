8f29fcbb86241dd4a335e9228d169df33b2e53be
Edwin Orlando Castro <edwinorlandocastro@hotmail.com>
2023-11-16 11:34:34 -0500

    Se realiza la configuracion de la paguina SignIn

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,7 +3,37 @@ import { createContext, useState, useEffect } from 'react'
 
  export const ShoppingCartContext = createContext()
 
+ export const initializeLocalStorage = () => {
+   const accountInLocalStorage = localStorage.getItem('account')
+   const signOutInLocalStorage = localStorage.getItem('sign-out')
+
+   let parsedAccount
+   let parsedSignOut
+
+   if(! accountInLocalStorage){
+      localStorage.setItem('account',JSON.stringify({}))
+      parsedAccount = {}
+
+   }else{
+      parsedAccount = JSON.parse(accountInLocalStorage)
+   }
+
+   if(!signOutInLocalStorage){
+      localStorage.setItem('sign-out', JSON.stringify(false))
+      parsedSignOut = false
+
+   } else{
+      parsedSignOut = JSON.parse(signOutInLocalStorage)
+   }
+ }
+
 export const ShoppingCartProvider = ({ children }) => {
+
+     // My account
+   const [account, setAccount] = useState({})
+   
+    //Sign out
+   const [signOut, setSignOut] = useState(false)
 
     //Shopping Cart . Increment quantity
     const [count, setCount] = useState(0)
@@ -116,6 +146,10 @@ export const ShoppingCartProvider = ({ children }) => {
             setFilteredItems,
             searchByCategory,
             setSearchByCategory,
+            account, 
+            setAccount,
+            signOut, 
+            setSignOut
 
 
         }}>
